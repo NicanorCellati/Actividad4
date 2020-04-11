281a567384ad514efb81f66253baed29a16cd835
Nicanor Cellati <garrotino28@gmail.com>
2020-04-11 18:33:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/actividad4/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.actividad4;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn=(Button)findViewById(R.id.tomarFoto);
+        image=(ImageView)findViewById(R.id.imageView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap=(Bitmap)data.getExtras().get("data"); //Este seria el dato que deberiamos mandar si queremos compartir la foto con un contacto de wpp o lo que sea
+        image.setImageBitmap(bitmap);
+    }
+}
